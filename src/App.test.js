c59d0b3698930aa7055bf57c7b87e2132877d922
suyeon <developer.ssu.ssu@gmail.com>
2023-09-28 18:21:02 +0900

    feat : 설명 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// 3개의 인자를 받는다. 테스트명, 함수, timeout
+// test는 3개의 인자를 받는다. 테스트명, 함수, timeout: 옵셔널
 // test('renders learn react link', () => {
 //   // render 는 특정 컴퍼넌트를 받아서 Virtual Dom을 생성해준다. screen 객체의 특정 메소드를 이용해서 객체에 접근할 수 있다.
 //   // 대소문자 구분이 없다.
